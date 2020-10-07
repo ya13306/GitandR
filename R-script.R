bbae3d7bb30d378fea583ea63163f46966de93a1
Zheng <yaxin.zheng@convatec.com>
2020-10-07 11:12:17 -0400

    test script

---
+++ R-script.R
@@ -0,0 +1,12 @@
+### this is testing current git version control
+
+
+#==========================================================================================
+#
+#
+#                                Complaints Typing App
+#                                Last update: SEP 2020
+#                               NOTE: updated type 6 RMC & TW8.7; TW8.3 columns
+#                                    version: 1.4
+#
+#==========================================================================================
